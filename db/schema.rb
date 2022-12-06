3d742b9b6c6fe8593923fdb2fec0e7ee752a5620
Maksim Nezhinskii <maksimnezhin@gmail.com>
2022-12-06 16:44:38 +0300

    Added login column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_04_124210) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_06_134036) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_04_124210) do
     t.string "nickname"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "login"
   end
 
   add_foreign_key "games", "users", column: "murdered_id"

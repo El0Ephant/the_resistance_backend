bb4930b7f36eb857f37a76fc6d371101a2220a5c
Aleksandr Kalinin <akalinin@sfedu.ru>
2022-12-10 10:09:31 +0300

    Authentication and Authorization

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_134036) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_09_052503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_134036) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["murdered_id"], name: "index_games_on_murdered_id"
+  end
+
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
   end
 
   create_table "participations", force: :cascade do |t|

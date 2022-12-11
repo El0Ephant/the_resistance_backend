a42dcc0efc7d36507a797280f3934b64f648400c
nezhinskii <maksimnezhin@gmail.com>
2022-12-11 15:43:21 +0300

    Renamed password column to work with devise

---
+++ db/migrate/20221211113317_change_password_column_name.rb
@@ -0,0 +1,5 @@
+class ChangePasswordColumnName < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :users, :password_digest, :encrypted_password
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_09_052503) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_11_113317) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_09_052503) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password_digest"
+    t.string "encrypted_password"
     t.string "nickname"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

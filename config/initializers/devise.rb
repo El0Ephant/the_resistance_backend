c6eef9aa53db77a1573d309882a5654ecce22db8
Maksim Nezhinskii <maksimnezhin@gmail.com>
2022-12-12 11:12:28 +0300

    fixed jwt processing

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,10 +14,10 @@ Devise.setup do |config|
   config.jwt do |jwt|
     jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
     jwt.dispatch_requests = [
-      ['POST', %r{^/api/login$}]
+      ['POST', %r{^/users/sign_in$}]
     ]
     jwt.revocation_requests = [
-      ['DELETE', %r{^/api/logout$}]
+      ['DELETE', %r{^/users/sign_out$}]
     ]
     jwt.expiration_time = 1.day.to_i
   end

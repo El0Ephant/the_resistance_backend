64804901f8392ecf20de816f64cceea173835f57
Maksim Nezhinskii <maksimnezhin@gmail.com>
2022-12-23 00:08:34 +0300

    Mongo magic

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,5 @@ module TheResistance
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    Mongoid.load!('config/mongoid.yml')
   end
 end

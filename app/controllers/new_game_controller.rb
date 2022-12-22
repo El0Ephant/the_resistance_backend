08a17c8a16940b550ff1466350fbebb189230452
Ksum7 <xamchik02@gmail.com>
2022-12-22 22:07:19 +0300

    another fix

--- app/controllers/new_game_controller.rb
+++ app/controllers/new_game_controller.rb
@@ -1,7 +1,6 @@
 class NewGameController < MembersController
-  include GameStateHelper
   def new_game
-    game_id = create_game params[:roles].size, params[:roles]
+    game_id = GameStateHelper::create_game params[:roles].size, params[:roles]
     render json:{
       gameId: game_id
     }

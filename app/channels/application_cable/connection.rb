b1510411f6e3779e25fed5a1821b17f665d5e494
nezhinskii <maksimnezhin@gmail.com>
2022-12-25 13:00:20 +0300

    Changed token extraction method one more time

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -7,7 +7,8 @@ module ApplicationCable
     def find_verified_user
       # request.params[:token] or something like: request.authorization.split(' ')[1]
       begin
-        jwt_payload = JWT.decode(request.authorization.split(' ')[1],
+        puts request.headers.to_h
+        jwt_payload = JWT.decode(request.headers[:Authorization].split(' ')[1],
                                  ENV['DEVISE_JWT_SECRET_KEY']).first
       rescue JWT::ExpiredSignature
         logger.error "A connection attempt was rejected due to an expired token"

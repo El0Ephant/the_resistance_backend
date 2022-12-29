48aebb4415e132f45374e7088daa8a27661a8274
Maksim Nezhinskii <maksimnezhin@gmail.com>
2022-12-29 18:27:01 +0300

    Fixed leadership transfer

--- app/helpers/game_state_helper.rb
+++ app/helpers/game_state_helper.rb
@@ -194,7 +194,7 @@ module GameStateHelper
       game_state.candidates = []
       game_state.votes_for_candidates = {}
       game_state.state = State::VOTE_FOR_CANDIDATES
-      game_state.leader_id = game_state.players[game_state.players.index(game_state.leader_id) + 1]
+      game_state.leader_id = game_state.players[(game_state.players.index(game_state.leader_id) + 1) % game_state.players.size]
     end
     game_state.save
     create_state_hash(game_state)
@@ -244,7 +244,7 @@ module GameStateHelper
     game_state.candidates = []
     game_state.votes_for_candidates = {}
     game_state.votes_for_result = {}
-    game_state.leader_id = game_state.players[game_state.players.index(game_state.leader_id) + 1]
+    game_state.leader_id = game_state.players[(game_state.players.index(game_state.leader_id) + 1) % game_state.players.size]
     game_state.save
     create_state_hash(game_state)
   end
